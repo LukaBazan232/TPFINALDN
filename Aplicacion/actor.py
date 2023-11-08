855e5853b1e77aaa76bad20f0a8606e5e48d73b5
lukabazan232 <lukabazan404@gmail.com>
2023-11-08 12:08:51 -0300

    punto 6

--- Aplicacion/actor.py
+++ Aplicacion/actor.py
@@ -11,8 +11,9 @@ bp = Blueprint('actor', __name__,url_prefix="/actor/")
 def index():
     db = get_db()
     actores = db.execute(
-        """ SELECT first_name as nombre , last_name as apellido FROM film f
-             join film_actor fa on f.film_id = fa.film_id 
-             join actor a on fa.actor_id = a.actor_id"""
+        """ SELECT f.film_id, f.title FROM film f
+            join film_actor fa on f.film_id = fa.film_id 
+            join actor a on fa.actor_id = a.actor_id
+            WHERE a.actor_id = ?"""
     ).fetchall()
     return render_template('actor/index.html', actores = actores)
